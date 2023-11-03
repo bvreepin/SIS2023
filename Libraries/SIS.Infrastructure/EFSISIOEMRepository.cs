69e2101f6b90de259f3eac9e10d76f8894c83993
SteijeDeBroek <steije.debrucker@student.hogent.be>
2023-11-03 15:27:38 +0100

    Aanpassingen IOEM
    hetzelfde als bij de Adressen, tot een punt dat ik zeker was al aangevuld

--- Libraries/SIS.Infrastructure/EFSISIOEMRepository.cs
+++ Libraries/SIS.Infrastructure/EFSISIOEMRepository.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using SIS.Domain;
 using SIS.Domain.Interfaces;
 using SIS.Infrastructure.EFRepository.Context;
 using System;
@@ -15,6 +17,57 @@ namespace SIS.Infrastructure
         private readonly ILogger<EFSISIOEMRepository> _logger;
         private readonly IConfiguration _configuration;
         private readonly SisDbContext _context;
+
+        private Dictionary<string, IOEM> _IOEMs;
+
+        public EFSISIOEMRepository(ILogger<EFSISIOEMRepository> logger, IConfiguration configuration, SisDbContext context)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _context = context;
+
+            // Laad alle studenten bij aanmaken van de repository
+            RefreshIOEM();
+        }
+
+        public Dictionary<string, IOEM> IOEMs
+        {
+            get
+            {
+                if (_IOEMs != null) return _IOEMs;
+                return RefreshIOEM();
+            }
+        }
+
+        public Dictionary<string, IOEM> RefreshIOEM()
+        {
+            _IOEMs = new Dictionary<string, IOEM>();
+            // vanaf hier nog niet aangepast
+            var dbStudents = _context.Students.Include(s => s.RegistrationState).ToList();
+
+            foreach (var student in dbStudents)
+            {
+                var p = _personRepository.Persons.Values.Where(person => person.FirstName == student.Person.FirstName && student.Person.LastName == student.Person.LastName).FirstOrDefault();
+                if (p == null)
+                {
+                    p = new Person { FirstName = student.Person.FirstName, LastName = student.Person.LastName };
+                    _personRepository.Insert(p);
+                };
+                var s = new Student
+                {
+                    Firstname = p.FirstName,
+                    LastName = p.LastName,
+                    Mobile = string.IsNullOrEmpty(student.Mobile) ? p.Mobile : student.Mobile,
+                    Email = string.IsNullOrEmpty(student.Email) ? p.Email : student.Email,
+                    OfficialCode = student.OfficialCode,
+                    RegistrationState = student.RegistrationState.Description
+
+                };
+                _students.Add(p.FirstName + " " + p.LastName, s);
+            }
+
+            return _students;
+        }
 
         public void Update(Student student)
         {
