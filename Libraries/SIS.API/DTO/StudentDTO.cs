f24fa60f2bb5a4e6b9ce0cf07b1b1855eb32d2dd
SteijeDeBroek <steije.debrucker@student.hogent.be>
2023-11-06 16:28:37 +0100

    StudentDTO

--- Libraries/SIS.API/DTO/StudentDTO.cs
+++ Libraries/SIS.API/DTO/StudentDTO.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SIS.API.DTO
 {
-    internal class StudentDTO
+    public class StudentDTO
     {
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
     }
 }
