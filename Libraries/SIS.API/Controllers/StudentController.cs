90def693f5747cc1ea3e3f1ae541ffb5a34fdced
SteijeDeBroek <steije.debrucker@student.hogent.be>
2023-11-06 16:15:05 +0100

    Json van Students

---
+++ Libraries/SIS.API/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SIS.API.DTO;
+using SIS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> _logger;
+        private readonly ISISStudentRepository _repository;
+        private readonly IMapper _mapper;
+
+        public StudentController(ILogger<StudentController> logger, ISISStudentRepository repository, IMapper mapper)
+        {
+            _logger = logger;
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet(Name = "GetStudents")]
+        public ActionResult<IEnumerable<StudentDTO>> Get()
+        {
+            return Ok(_mapper.Map<List<StudentDTO>>(_repository.Students.Values.ToList()));
+        }
+    }
+        
+}
